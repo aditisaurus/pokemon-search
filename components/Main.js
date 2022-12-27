40e1b48ad8f7fda668215afa3d817424e55f9932
aditisinghdeveloper <aditisinghdeveloper@gmail.com>
2022-12-27 21:05:44 +0530

    SearchResult

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
+import SearchResult from "./SearchResult";
 import { FixedSizeGrid as Grid } from "react-window";
-import { ReactSearchAutocomplete } from "react-search-autocomplete";
 
-import Image from "next/image";
 
 function Main() {
   let historyList = [];
@@ -91,28 +90,7 @@ function Main() {
           ))}
         </ul>
       </div>
-
-      {pokeData?.map((value) => (
-        <div
-          class="max-w-sm rounded overflow-hidden shadow-sm"
-          data-cy="pokemon-list"
-        >
-          <Image
-            class="rounded-t-lg"
-            src={
-              `https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/` +
-              parseInt(value.url.split("/").slice(-2, -1)) +
-              `.svg`
-            }
-            width={100}
-            height={100}
-            alt=""
-          />
-          <div class="px-6 py-4">
-            <div class="font-bold text-white text-xl mb-2">{value.name} </div>
-          </div>
-        </div>
-      ))}
+      <SearchResult pokeData={pokeData} />
     </div>
   );
 }
